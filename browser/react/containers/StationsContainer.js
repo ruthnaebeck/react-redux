71a00a3dc6b2178e3ee7b8cf87ff358c408027e2
Nitika Nadgar <nitiquita@gmail.com>
2017-04-03 12:57:18 -0400

    connect function

---
+++ browser/react/containers/StationsContainer.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import Stations from '../components/Stations'
+import {connect} from 'react-redux'
+
+class StationsContainer extends Component {
+    constructor(props){
+        super(props)
+
+    };
+    
+    render() {
+    console.log("props", props)
+    return <Stations />
+    }
+}
+
+const convertSongsToStations = function (songsArray) {
+    let stations = {};
+    songsArray.forEach(function(song) {
+        let genre = song.genre
+        if (stations[genre]) {
+            stations[genre].push(song);
+        } else {
+            stations[genre] = [song];
+        }
+    });
+    return stations;
+};
+
+const mapStateToProps = (state) => ({stations: convertSongsToStations(state.songs)})
+
+const mapDispatchToProps = (dispatch) => ({})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Stations)
+// export default StationsContainer
